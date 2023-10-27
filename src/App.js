86c1899b3d56a9b98b032f7e2395e33228037be4
0122345 <ntwarifiacre043@gmail.com>
2023-10-27 20:56:02 +0200

    initial commiting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react';
+ 
+  import { data } from "./imaging";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+     <h1>
+      yoo you there!
+     </h1>
+
+     <div className=''>
+      {data.map((item)=> {
+        <img src={item.img} alt='/'/> 
+      })}
+
+     </div>
+     </>
   );
 }
-
+   
 export default App;
---
+++ src/imaging.js
@@ -0,0 +1,35 @@
+ 
+export const data = [
+    {
+        id: 1,
+        img: ".img\wallpaperflare.com_wallpaper.jpg"
+    },
+    {
+        id: 2,
+        img: ".img\wallpaperflare.com_wallpaper\(1\).jpg"
+    },
+    {
+        id: 3,
+        img: ".img\wallpaperflare.com_wallpaper\(2\).jpg"
+    },
+    {
+        id: 4,
+        img: ".img\wallpaperflare.com_wallpaper\(3\).jpg"
+    },
+    {
+        id: 5,
+        img: ".img\wallpaperflare.com_wallpaper\(4\).jpg"
+    },
+    {
+        id: 6,
+        img: ".img\wallpaperflare.com_wallpaper\(5\).jpg"
+    },
+    {
+        id: 7,
+        img: ".img\wallpaperflare.com_wallpaper\(6\).jpg"
+    },
+    {
+        id: 8,
+        img: ".img\wallpaperflare.com_wallpaper\(7\).jpg"
+    }
+]
