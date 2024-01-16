6a09344256294c6eaff91c31b41c3e0d6d5faf97
0122345 <ntwarifiacre043@gmail.com>
2024-01-16 21:00:01 +0200

    end of the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
- import React from 'react' 
-import { data } from "./components/Imaging"
+ import {MdChevronLeft,MdChevronRight} from 'react-icons/md';
+import { data } from "./components/Imaging";
 
 function App() {
+    
+  const slideLeft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 600
+  }
+
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 600
+  }
+
+
+
+
   return (
      <>
-     <img className='w-full h-[360px] bg-auto object-cover' src="https://motionbgs.com/media/1474/rengoku-in-fire-demon-slayer.jpg" alt="" />
+     <img className='w-full bg-slate-500 h-[360px] bg-auto object-contain' src="./img/wallpaperflare.com_wallpaper(6).jpg" alt="" />
 
-     {/* <div className='relative flex items-center'>
-
-      {data.map((item)=> {
-        <img className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in duration-300' src={item.img} alt='/'/> 
-      })}
-
-     </div> */}
-
-     <>
-     <img src="./img/wallpaperflare.com_wallpaper.jpg" alt="sinz" />
-     </>
+     <div className='relative flex items-center'>
+      <MdChevronLeft onClick={slideLeft} className='opacity-50 cursor-pointer hover:opacity-100'  size={50} />
+      <div 
+      id="slider"
+      className='w-full h-full overflow-scroll scroll whitespace-nowrap scroll-smooth  scrollbar-hide md:scrollbar-default'
+        >
+         {data.map((item) => {
+      return  <img 
+        className='w-[400px] inline-block p-2 cursor-pointer hover:scale-105 ease-in duration-300'
+        src={item.img}
+        alt='/'
+        />
+       })}
+      </div>
+      <MdChevronRight onClick={slideRight } className='opacity-50 cursor-pointer hover:opacity-100' size={50} />
+     </div>
      </>
   );
 }
--- src/components/Imaging.js
+++ src/components/Imaging.js
@@ -2,34 +2,34 @@
 export const data = [
     {
         id: 1,
-        img: ".img/wallpaperflare.com_wallpaper.jpg"
+        img: "./img/wallpaperflare.com_wallpaper.jpg"
     },
     {
         id: 2,
-        img: ".img/wallpaperflare.com_wallpaper/(1/).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(1).jpg"
     },
     {
         id: 3,
-        img: ".img\wallpaperflare.com_wallpaper\(2\).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(2).jpg"
     },
     {
         id: 4,
-        img: ".img\wallpaperflare.com_wallpaper\(3\).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(3).jpg"
     },
     {
         id: 5,
-        img: ".img\wallpaperflare.com_wallpaper\(4\).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(4).jpg"
     },
     {
         id: 6,
-        img: ".img\wallpaperflare.com_wallpaper\(5\).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(5).jpg"
     },
     {
         id: 7,
-        img: ".img\wallpaperflare.com_wallpaper\(6\).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(6).jpg"
     },
     {
         id: 8, 
-        img: ".img\wallpaperflare.com_wallpaper\(7\).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(7).jpg"
     }
 ]
