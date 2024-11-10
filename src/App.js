4cf045f90def4c9fd5301580cf584ee357d20c85
0122345 <ntwarifiacre043@gmail.com>
2024-11-10 01:48:13 -0800

    play with it

--- src/App.js
+++ src/App.js
@@ -1,45 +1,56 @@
- import {MdChevronLeft,MdChevronRight} from 'react-icons/md';
- import { data } from './components/Imaging';
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { data } from "./components/Imaging";
 
- 
 function App() {
-    
   const slideLeft = () => {
-    var slider = document.getElementById('slider');
-    slider.scrollLeft = slider.scrollLeft - 600
-  }
-
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 600;
+  };
 
   const slideRight = () => {
-    var slider = document.getElementById('slider');
-    slider.scrollLeft = slider.scrollLeft + 600
-  }
-
-
-
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 600;
+  };
 
   return (
-     <>
-     <img className='w-full bg-black h-[360px] bg-auto object-contain' src="./img/wallpaperflare.com_wallpaper(6).jpg" alt="" />
+    <>
+      <img
+        className="w-full bg-black h-[360px] bg-auto object-contain"
+        src="./img/wallpaperflare.com_wallpaper(6).jpg"
+        alt=""
+      />
 
-     <div className='relative flex items-center'>
-      <MdChevronLeft onClick={slideLeft} className='opacity-50 cursor-pointer hover:opacity-100'  size={50} />
-      <div 
-      id="slider"
-      className='w-full h-full overflow-hidden  scroll whitespace-nowrap scroll-smooth  scrollbar-hide md:scrollbar-default'
-        >
-         {data.map((item) => {
-      return  <img 
-        className='w-[400px] inline-block p-2 cursor-pointer hover:scale-105 ease-in duration-300'
-        src={item.img}
-        alt='/'
+      <div className="relative flex items-center">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          size={50}
         />
-       })}
+        <div
+          id="slider"
+          className="w-full h-full overflow-x-auto scroll whitespace-nowrap scroll-smooth scrollbar-hide md:scrollbar-default"
+        >
+          {data.map((item) => (
+            <div key={item.id} className="relative inline-block">
+              <img
+                className="max-w-[550px] w-full inline-block p-2 cursor-pointer hover:scale-105 ease-in duration-300"
+                src={item.img}
+                alt="/"
+              />
+              <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg text-white hover:text-xl hover:text-blue-400 hover:scale-125 transition-all duration-300 font-bold drop-shadow-lg">
+                {item.name}
+              </p>
+            </div>
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          size={50}
+        />
       </div>
-      <MdChevronRight onClick={slideRight } className='opacity-50 cursor-pointer hover:opacity-100' size={50} />
-     </div>
-     </>
+    </>
   );
 }
-   
+
 export default App;
--- src/components/Imaging.js
+++ src/components/Imaging.js
@@ -2,34 +2,43 @@
 export const data = [
     {
         id: 1,
-        img: "./img/wallpaperflare.com_wallpaper.jpg"
+        img: "./img/wallpaperflare.com_wallpaper.jpg",
+        name: "Kyojuro"
+
     },
     {
         id: 2,
-        img: "./img/wallpaperflare.com_wallpaper(1).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(1).jpg",
+        name: "Toge Inumaki"
     },
     {
         id: 3,
-        img: "./img/wallpaperflare.com_wallpaper(2).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(2).jpg",
+        name: "Ichigo Kurosaki"
     },
     {
         id: 4,
-        img: "./img/wallpaperflare.com_wallpaper(3).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(3).jpg",
+        name: "Kurosaki Ichigo"
     },
     {
         id: 5,
-        img: "./img/wallpaperflare.com_wallpaper(4).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(4).jpg",
+        name: "Gutz Berserk"
     },
     {
         id: 6,
-        img: "./img/wallpaperflare.com_wallpaper(5).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(5).jpg",
+        name: "Dodo something"
     },
     {
         id: 7,
-        img: "./img/wallpaperflare.com_wallpaper(6).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(6).jpg",
+        name: "Sasangyeyo Attack on Titan"
     },
     {
         id: 8, 
-        img: "./img/wallpaperflare.com_wallpaper(7).jpg"
+        img: "./img/wallpaperflare.com_wallpaper(7).jpg",
+        name: "Ruokin Kenshin Himura"
     }
 ]
